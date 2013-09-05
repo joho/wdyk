9bc92fa9b15dd66f77e29540013d73f91131fcf8
John Barton (joho) <jrbarton@gmail.com>
2013-09-05 17:12:35 +1000

    Generated by generator-bespoke

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,23 @@ bespoke.horizontal.from('article', {
   bullets: 'li, .bullet',
   hash: true
 });
+
+var countdownTimer = function(e) {
+  var start = new Date().getTime();
+
+  setInterval(function () {
+    var now = new Date().getTime();
+    var rawSeconds = parseInt((now - start) / 1000);
+    var minutes = parseInt(rawSeconds / 60);
+    var seconds = 60 - (rawSeconds % 60)
+    e.target.innerHTML = (5 - minutes) + ":" + seconds;
+  }, 1000);
+};
+
+var el = document.getElementById("clock");
+if (el.addEventListener) {
+    el.addEventListener("click", countdownTimer, false);
+} else {
+    el.attachEvent('onclick', countdownTimer);
+}  
+
